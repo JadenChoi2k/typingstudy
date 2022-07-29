ec151065095fbd2f5228aa8bb1ec531f3696b4aa
Jaden Choi <jkya02@gmail.com>
2022-07-29 19:17:09 +0900

    add validation to UserDto

--- src/main/java/com/typingstudy/interfaces/user/UserDto.java
+++ src/main/java/com/typingstudy/interfaces/user/UserDto.java
@@ -2,34 +2,46 @@ package com.typingstudy.interfaces.user;
 
 import lombok.Data;
 
+import javax.validation.constraints.*;
 import java.time.LocalDateTime;
 
 public class UserDto {
 
     @Data
     public static class LoginRequest {
+        @NotNull(message = "이메일을 입력해주세요")
         private String email;
+        @NotNull(message = "비밀번호를 입력해주세요")
         private String password;
     }
 
     @Data
     public static class JoinRequest {
+        @Email(message = "이메일 형식으로 기재해주시기 바랍니다")
         private String email;
+        @Max(value = 30, message = "비밀번호는 30자 이하입니다.")
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+                message = "비밀번호는 8자 이상의 영문자, 숫자 조합으로 이루어집니다.")
         private String password;
+        @Max(value = 20, message = "유저 이름은 20자 이하입니다")
+        @Pattern(regexp = "[가-힣A-Za-z][가-힣A-Za-z\\d]+",
+                message = "유저 이름은 2자 이상의 영문자, 한글, 숫자로 이루어집니다.")
         private String username;
         private String profileUrl;
     }
+
+    @Data
+    public static class CreateFavoriteGroupRequest {
+        @Size(min = 1, max = 50, message = "그룹 이름은 1에서 50자 이내입니다.")
+        private String groupName;
+    }
+
 
     @Data
     public static class Main {
         private String id;
         private String name;
         private String profileUrl;
-    }
-
-    @Data
-    public static class CreateFavoriteGroupRequest {
-        private String groupName;
     }
 
     @Data
